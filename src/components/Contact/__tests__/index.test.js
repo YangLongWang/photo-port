a87db8f3c558fdc4e97ae4544b56cd7b3b6a7c69
yang <joey674901205@gmail.com>
2022-09-11 17:42:23 -0400

    test the component

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,30 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Contact from '..';
+
+afterEach(cleanup);
+
+describe('render contact', () => {
+  it('ContactForm is rendering properly', () => {
+    render(<Contact />);
+  });
+
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Contact />)
+    expect(asFragment()).toMatchSnapshot()
+  });
+
+  it('match the text content to be Contact me', () => {
+    const { getByTestId } = render(<Contact />);
+  
+      expect(getByTestId('h1tag')).toHaveTextContent('Contact me');
+    });
+    
+    it('match the text content to be Submit', () => {
+      const { getByTestId } = render(<Contact />);
+      
+      expect(getByTestId('buttontag')).toHaveTextContent('Submit');
+    });
+});
